4fa5f450adbade45e6cec757977f3147840628a8
Оксана <mari-mari@ukr.net>
2017-07-26 22:33:08 +0300

    change models,database,db,schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+	validates :login, presence: true, length: { maximum: 15 }  
+	validates :password, presence: true, length: { maximum: 8 }
+	validates :email, presence: true
+	validates :birthdate, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :role_id, presence: true
+	validates :level, presence: true
 end
--- db/migrate/20170726112826_create_users.rb
+++ db/migrate/20170726112826_create_users.rb
@@ -1,19 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.integer :id
+      
       t.string :login
       t.string :password
       t.string :email
       t.date :birthdate
       t.string :first_name
       t.string :last_name
-      t.date_time :confirmation_time
+      t.datetime :confirmation_time
       t.integer :role_id
       t.integer :confirmation_by_admin_id
       t.integer :level
-      t.date_time :end_time
-      t.date_time :penalty_time
+      t.datetime :end_time
+      t.datetime :penalty_time
 
       t.timestamps
     end
