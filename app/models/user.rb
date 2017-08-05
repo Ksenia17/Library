4fa1a1af07a605569b28d784dc84b3c3d6a89f2a
Оксана <mari-mari@ukr.net>
2017-08-06 00:26:15 +0300

    add  gem  devise, change  model User, validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
-	validates :login, presence:true, length: { maximum: 10 }
-	validates :password ,  presence: true, length: { maximum: 8 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :login, presence:true, length: { maximum: 10 }
+  validates :reset_password_token,  presence: true, length: { maximum: 8 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     validates :birthdate  ,  presence: true 
@@ -9,5 +15,5 @@ class User < ApplicationRecord
     validates :last_name  ,  presence: true
     validates :role_id    ,  presence: true 
     validates :level      ,  presence: true              
-
+       
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730094737) do
+ActiveRecord::Schema.define(version: 20170805205117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,20 +71,30 @@ ActiveRecord::Schema.define(version: 20170730094737) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.string "login"
-    t.string "password"
-    t.string "email"
     t.date "birthdate"
     t.string "first_name"
     t.string "last_name"
-    t.datetime "confirmation_time"
+    t.datetime "confirmation_time", null: false
     t.integer "role_id"
     t.integer "confirmation_by_admin_id"
     t.integer "level"
-    t.datetime "end_time"
-    t.datetime "penalty_time"
+    t.datetime "end_time", null: false
+    t.datetime "penalty_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
