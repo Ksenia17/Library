03893af27187b5b947f42cd8122926aad2497abd
Оксана <mari-mari@ukr.net>
2017-08-08 22:26:15 +0300

    add controller users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :login, presence:true, length: { maximum: 10 }
+#  validates :login, presence:true, length: { maximum: 10 }
   validates :reset_password_token,  presence: true, length: { maximum: 8 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-    validates :birthdate  ,  presence: true 
-    validates :first_name ,  presence: true
-    validates :last_name  ,  presence: true
-    validates :role_id    ,  presence: true 
-    validates :level      ,  presence: true              
+#    validates :birthdate  ,  presence: true 
+#    validates :first_name ,  presence: true
+#    validates :last_name  ,  presence: true
+#    validates :role_id    ,  presence: true 
+#    validates :level      ,  presence: true              
        
 end
