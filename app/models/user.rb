657024369f1830c835ddb1c7e366c6519d2009fa
Оксана <mari-mari@ukr.net>
2017-08-17 22:16:52 +0300

    delete controller users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,28 +7,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-#  validates :login, presence:true, length: { maximum: 10 }
-#  validates :encrypted_password,  presence: true, length: { maximum: 8 }
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     validates :birthdate  ,  presence: true 
     validates :first_name ,  presence: true
     validates :last_name  ,  presence: true
-#    validates :role_id    ,  presence: true 
 #    validates :level      ,  presence: true   
 
  before_create :create_role
 
   private
     def create_role
-      self.roles << Role.find_by_name(:user) if self.roles.blank?
-      
-  #    self.Role = Role.find_by_name('user') # ? присвоить значение user - по умолчанию
-      
-    end
-
-
-       
+      self.roles << Role.find_by_name(:user) if self.roles.blank?      
+    end       
 end
