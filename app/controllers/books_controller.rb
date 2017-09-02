f1ce20d9b7d463c4525cb101829e38aebe5e6be8
Оксана <mari-mari@ukr.net>
2017-09-02 19:57:39 +0300

    change routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
-  before_filter :authenticate_user! # for devise
-  load_and_authorise_resource # for cancancan
-
+  before_action :authenticate_user! # for devise
+#  load_and_authorise_resource # for cancancan
 
   def index
   end
+
+  def create    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,34 +6,40 @@ Rails.application.routes.draw do
               controllers: {
                 sessions: 'users/sessions' 
                           }
- 
-  get 'admins/index' #, as: 'user_root'
+ namespace :admins do
+  get 'index'
+ end
+
+#  get 'admins/index' 
 
   root to: "home#index"
   get 'home/index'
 
-
   resources :users do
     resources :books
-    resources :book_histories
-    resources :fines
-    resources :book_items
-    resources :book_requests
-    resources :users_roles
-    end
-  resources :books do
-    resources :book_items
-    resources :book_requests
-  end 
-  resources :book_items do
-    resources :book_histories
-  end 
-  resources :book_requests do
-    resources :book_histories
-  end 
-  resources :book_types do
-    resources :books
-  end 
+  end
+
+#  resources :users do
+#    resources :books
+#    resources :book_histories
+#    resources :fines
+#    resources :book_items
+#    resources :book_requests
+#    resources :users_roles
+#    end
+#  resources :books do
+#    resources :book_items
+#    resources :book_requests
+#  end 
+#  resources :book_items do
+#    resources :book_histories
+#  end 
+#  resources :book_requests do
+#    resources :book_histories
+#  end 
+#  resources :book_types do
+#    resources :books
+#  end 
    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
