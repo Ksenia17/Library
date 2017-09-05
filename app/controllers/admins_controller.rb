cb649bec4283d248c43cb145813703111d28ca48
Оксана <mari-mari@ukr.net>
2017-09-05 23:51:26 +0300

    create first menu

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,7 +3,17 @@ class AdminsController < ApplicationController
   layout "admin"
 
   def index
-    @users = User.all
+  
+  #  @users = User.order("first_name")
+
+   if params[:confirmation_by_admin_id]
+    @users = User.where(["confirmation_by_admin_id != ?", nil]) 
+
+   #  @users = User.where(:confirmation_by_admin_id => (params[:confirmation_by_admin_id] != 0))   
+    end    
     
+  
+  
+ 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :users_roles
   has_many :roles, :through => :users_roles
   has_one  :book
