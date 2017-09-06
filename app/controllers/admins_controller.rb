ad6ba57ee87f49343c2892ec6b992f666d91f849
Оксана <mari-mari@ukr.net>
2017-09-06 20:42:44 +0300

    create first menu-2

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,10 +3,12 @@ class AdminsController < ApplicationController
   layout "admin"
 
   def index
-  
+  #  @users = User.all
   #  @users = User.order("first_name")
-
+  #  binding.pry
+  
    if params[:confirmation_by_admin_id]
+    
     @users = User.where(["confirmation_by_admin_id != ?", nil]) 
 
    #  @users = User.where(:confirmation_by_admin_id => (params[:confirmation_by_admin_id] != 0))   
