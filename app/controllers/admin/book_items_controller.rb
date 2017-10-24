e15ef6aaf0791ae1849f509bb89f93e91ebfb2dc
Оксана <mari-mari@ukr.net>
2017-10-24 22:53:58 +0300

    change menu item'

--- app/controllers/admin/book_items_controller.rb
+++ app/controllers/admin/book_items_controller.rb
@@ -3,50 +3,33 @@ class Admin::BookItemsController < ApplicationController
  load_and_authorize_resource :book_item
  
  layout "admin"
-
-  def new
-    @book = Book.find(params[:book_id])
-    @book_item =@book.book_items.build
-    
-  end
+  
 
   def create
    
-   binding.pry
+  
      @book = Book.find(params[:book_id])
-     @book_item = @book.book_items.build  #(item_params)
+     @book_item = @book.book_items.build  
     
      @book_item.user_id = current_user.id
 
 
      if @book_item.save 
-      redirect_to admin_book_book_item_path(@book,@book_item),:notice => "New example was successfully created"  # update  # update
+      redirect_to admin_book_book_item_path(book_id: @book.id, id: @book_item.id),:notice => "New example was successfully created"  # update  # update
      end
 
 
   end
 
   def show
-    @book_item = BookItem.find(params[:book_id]) #название модели
+    
+    @book_item = BookItem.find(params[:id]) #название модели
   end
   
   def index
     @book = Book.find(params[:book_id])
     @book_items =  @book.book_items
   end  
-
-  def update
-    if @book_item.update 
-      redirect_to admin_book_book_item_path(@book,@book_item)  # update
-     end
-  end
-
-
-
-  private
-  def item_params
-   params.require(:book_item).permit(:book_id,:user_id,:archived_at)  # не получалось с одним параметром!
   
-  end
 
 end
--- app/helpers/admin/books_helper.rb
+++ app/helpers/admin/books_helper.rb
@@ -1,2 +1,5 @@
 module Admin::BooksHelper
+  def options_select_new
+    @book.name
+  end
 end
