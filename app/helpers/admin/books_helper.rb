d2fc24f3912f7244cad938c6d31dc2caede43d35
Оксана <mari-mari@ukr.net>
2017-10-27 22:27:27 +0300

    change menu item-2'

--- app/helpers/admin/books_helper.rb
+++ app/helpers/admin/books_helper.rb
@@ -1,5 +1,6 @@
 module Admin::BooksHelper
- # def options_select_new
- #   @book.name
- # end
+  def options_select_new
+   # @book.name
+    @book_type_options = BookType.all.collect{|p| [ p.name,p.id]}
+  end
 end
