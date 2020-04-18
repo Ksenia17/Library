dcb5de75b0e6c35faa710211ed7eec1cf1587358
Ksenia17 <mari-mari@ukr.net>
2020-04-18 23:20:12 +0300

    switching locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,88 +1,82 @@
 Rails.application.routes.draw do
-  
+ scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do  # ru|en
 
-  namespace :admin do
-    get 'fines/create'
-  end
+    # namespace :admin do
+    #   get 'fines/create'
+    # end
 
-#  namespace :reader do
-#    get 'book_histories/index'
-#  end
 
-#  namespace :admin do
-#    get 'book_types/index'
-#  end
 
-  devise_for :users,    
-    
-              controllers: {
-                sessions: 'users/sessions' 
-                           }
- root to: "home#index"
- 
- namespace :admin do
-
-   
-
-    resources :info, only: [:index,:show,:edit, :update] do  
-    end    
-
-    resources :users , only: [:index,:show,:edit, :update] do
-   
-      get 'list_wait', on: :collection  #
-      get 'wait',  on: :member
-      post 'confirm', on: :member # совпадает с именем def..end в контроллере
+    devise_for :users,    
       
-    #  get 'list_fines',on: :collection
-    #  post 'plus_fine', on: :collection
+                controllers: {
+                  sessions: 'users/sessions' 
+                             }
+   root to: "home#index"
+   
+   namespace :admin do
+
+     
+
+      resources :info, only: [:index,:show,:edit, :update] do  
+      end    
+
+      resources :users , only: [:index,:show,:edit, :update] do
+     
+        get 'list_wait', on: :collection  #
+        get 'wait',  on: :member
+        post 'confirm', on: :member # совпадает с именем def..end в контроллере
+        
+      #  get 'list_fines',on: :collection
+      #  post 'plus_fine', on: :collection
+      end  
+
+      resources :fines, only: [:index,:create] 
+
+      resources :book_requests, only: [:index] do 
+       
+        post 'approval', on: :member
+       
+        post 'negative', on: :member 
+
+        resources :book_history, only: [:new,:create]
+      end
+
+      resources :book_types , only: [:new,:create,:index,:show,:edit,:update,:destroy] do   
+      end 
+      
+      resources :books, only: [:new,:create,:index,:show,:edit, :update,:destroy]  do       
+        resources :book_items, only: [:create,:index,:show,:destroy] do
+          resources :book_histories, only: [:index] 
+
+        end  
+      end
+
+
+   end
+
+   namespace :reader do
+    
+    resources :book_requests, only: [:index]
+
+    resources :book_histories, only: [:index] do  
+      post 'return_book', on: :member
     end  
 
-    resources :fines, only: [:index,:create] 
 
-    resources :book_requests, only: [:index] do 
-     
-      post 'approval', on: :member
-     
-      post 'negative', on: :member 
+    resources :books , only: [:index] do 
+          
+      resources :book_requests, only: [:create]   
+              
+    end   
 
-      resources :book_history, only: [:new,:create]
-    end
-
-    resources :book_types , only: [:new,:create,:index,:show,:edit,:update,:destroy] do   
-    end 
+    resources :users , only: [:show,:edit, :update]  
     
-    resources :books, only: [:new,:create,:index,:show,:edit, :update,:destroy]  do       
-      resources :book_items, only: [:create,:index,:show,:destroy] do
-        resources :book_histories, only: [:index] 
+    resources :book_types , only: [:index]  
 
-      end  
-    end
+   end 
 
-
- end
-
- namespace :reader do
-  
-  resources :book_requests, only: [:index]
-
-  resources :book_histories, only: [:index] do  
-    post 'return_book', on: :member
-  end  
-
-
-  resources :books , only: [:index] do 
-        
-    resources :book_requests, only: [:create]   
-            
-  end   
-
-  resources :users , only: [:show,:edit, :update]  
-  
-  resources :book_types , only: [:index]  
-
- end 
-
-
+  end 
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
